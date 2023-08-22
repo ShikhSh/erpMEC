1ba80a3b901f16eb8bcd9649f886f0eee859e28c
ShikharSh <shikhar.sharma1496@gmail.com>
2023-08-22 01:07:21 -0400

    Update script.js to call end point.

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', async function() {
     const resellerCounterElement = document.getElementById('resellercounter');
     const resellerIncrementButton = document.getElementById('resellerButton');
     
-    let buyerCounter = await getCounterValue('buyerCnt.txt'); // Load initial value from Gist
-    let resellerCounter = await getCounterValue('resellerCnt.txt'); // Load initial value from Gist
+    let buyerCounter = 0;//await getCounterValue('buyerCnt.txt'); // Load initial value from Gist
+    let resellerCounter = 0;//await getCounterValue('resellerCnt.txt'); // Load initial value from Gist
 
     console.log('Adding listeners to buttons');
     if (buyerIncrementButton) {
@@ -14,9 +14,9 @@ document.addEventListener('DOMContentLoaded', async function() {
             event.preventDefault(); // Prevent default form submission
             console.log('Buyer listener called');
             buyerCounter++;
-            // buyerCounterElement.textContent = buyerCounter;
+            buyerCounterElement.textContent = buyerCounter;
             await updateCounterValue('b', buyerCounter); // Update value in Gist
-            window.location.href = 'buyers.html';
+            // window.location.href = 'buyers.html';
         });
     } else {
         console.error('No Buyer Button!!');
@@ -28,70 +28,77 @@ document.addEventListener('DOMContentLoaded', async function() {
             event.preventDefault(); // Prevent default form submission
             console.log('Reseller listener called');
             resellerCounter++;
-            // resellerCounterElement.textContent = resellerCounter;
+            resellerCounterElement.textContent = resellerCounter;
             await updateCounterValue('r', resellerCounter); // Update value in Gist
-            window.location.href = 're-sellers.html';
+            // window.location.href = 're-sellers.html';
         });
     } else {
         console.error('No Reseller Button!!');
     }
     console.log('Reseller listern added');
 
-    async function getCounterValue(file_name) {
-        console.log('Getting counter value');
-        try {
-            const response = await fetch('https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c', {
-                headers: {
-                    'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
-                },
-            });
-            const data = await response.json();
-            let val = data.files[file_name] ? parseInt(data.files[file_name].content) : 250;
-            console.log('Getting counter value');
-            return val;
-        } catch (error) {
-            console.error('Error getting counter value:', error);
-            return -1000;
-        }
-    }
+    // async function getCounterValue(file_name) {
+    //     console.log('Getting counter value');
+    //     try {
+    //         const response = await fetch('https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c', {
+    //             headers: {
+    //                 'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
+    //             },
+    //         });
+    //         const data = await response.json();
+    //         let val = data.files[file_name] ? parseInt(data.files[file_name].content) : 250;
+    //         console.log('Getting counter value');
+    //         return val;
+    //     } catch (error) {
+    //         console.error('Error getting counter value:', error);
+    //         return -1000;
+    //     }
+    // }
 
-    async function updateCounterValue(b_or_s, newValue) {
+    async function updateCounterValue(b_or_r, newValue) {
         console.log('Updating counter value');
         try {
-            if (b_or_s == 'b') {
-                const response = await fetch(`https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        files: {
-                            'buyerCnt.txt': {
-                                content: newValue.toString()
-                            }
-                        }
-                    })
-                });
-            } else {
-                const response = await fetch(`https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        files: {
-                            'resellerCnt.txt': {
-                                content: newValue.toString()
-                            }
-                        }
-                    })
-                });
-            }
-            if (!response.ok) {
-                console.error('Error updating counter value.');
-            }
+            // if (b_or_s == 'b') {
+            //     const response = await fetch(`https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c`, {
+            //         method: 'PATCH',
+            //         headers: {
+            //             'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
+            //             'Content-Type': 'application/json'
+            //         },
+            //         body: JSON.stringify({
+            //             files: {
+            //                 'buyerCnt.txt': {
+            //                     content: newValue.toString()
+            //                 }
+            //             }
+            //         })
+            //     });
+            // } else {
+            //     const response = await fetch(`https://api.github.com/gists/34b0932be525fa9e28bd897748fcd26c`, {
+            //         method: 'PATCH',
+            //         headers: {
+            //             'Authorization': 'Bearer ' + process.env.GH_PERSONAL_ACCESS_TOKEN,
+            //             'Content-Type': 'application/json'
+            //         },
+            //         body: JSON.stringify({
+            //             files: {
+            //                 'resellerCnt.txt': {
+            //                     content: newValue.toString()
+            //                 }
+            //             }
+            //         })
+            //     });
+            // }
+            // if (!response.ok) {
+            //     console.error('Error updating counter value.');
+            // }
+            const response = await fetch(`http://128.2.66.182:8080/store`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain'
+                },
+                body: b_or_r
+            });
         } catch (error) {
             console.error('Error updating counter value:', error);
         }
